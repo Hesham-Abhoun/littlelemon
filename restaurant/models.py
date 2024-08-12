263c993908f0b049c00cbd5cde79fdbd46724709
hesahm_ab_git <hesham.abhoun@gmail.com>
2024-08-12 20:53:35 +0000

    connect to the mariadb then add menu and booking models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    customer_name = models.CharField(max_length=255)
+    customer_email = models.EmailField()
+    booking_date = models.DateTimeField()
+    number_of_guests = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.booking_date}"
